9a63ff2ebc9b11c478c0215387fa9731165cd8f5
Diego Pagini <diegopagini777@gmail.com>
2020-12-21 06:18:43 -0300

    estructura lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/shared/home/home.component';
+import { AboutComponent } from './components/shared/about/about.component';
+import { HeroesComponent } from './components/shared/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
