a8dd76b6f7c2ca24f02a027d84a099b963a72ae0
Diego Pagini <diegopagini777@gmail.com>
2020-12-21 06:40:25 -0300

    router listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/shared/home/home.component';
+import { AboutComponent } from './components/shared/about/about.component';
+import { HeroesComponent } from './components/shared/heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
