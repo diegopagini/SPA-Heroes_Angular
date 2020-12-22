ac6d0836e5ddc8d08c788b99b447aafa769374dd
Diego Pagini <diegopagini777@gmail.com>
2020-12-22 05:38:26 -0300

    activatedRoute.params.subscribe es para el componente resultante de la navegacion

--- src/app/components/shared/heroe/heroe.component.ts
+++ src/app/components/shared/heroe/heroe.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+  styleUrls: ['./heroe.component.css'],
 })
 export class HeroeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  heroe: any = {};
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroeService: HeroesService
+  ) {
+    this.activatedRoute.params.subscribe((params) => {
+      // console.log(params['id']);
+      this.heroe = this.heroeService.getHeroe(params['id']);
+    });
   }
 
+  ngOnInit(): void {}
 }
